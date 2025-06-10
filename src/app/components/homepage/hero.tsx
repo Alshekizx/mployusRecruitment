de156c4862d093f0654be15f2f18503b8ab33be9
seyi <seyiduncan40@gmail.com>
2025-06-10 11:22:52 +0100

    lint fix

--- src/app/components/homepage/hero.tsx
+++ src/app/components/homepage/hero.tsx
@@ -3,19 +3,30 @@ import Image from "next/image";
 export default function Hero() {
   return (
     <div className="containerDiv flex flex-col lg:flex-row items-center justify-between gap-10">
-      <div className="flex flex-col gap-12 max-w-xl">
+      <div className="flex flex-col gap-4 max-w-xl">
+        <p className="text-[var(--primary-color)]">Comprehensive Talent Solutions</p>
         <h1>
-          Find Jobs & Hire Talent with{" "}
-          <span className="text-[var(--primary-color)]">MployUs</span>
+          Expert {" "} <span className="text-[var(--primary-color)]">Talent</span>{" "}
+          & {" "}<span className="text-[var(--primary-color)]">Candidate</span>{" "}
+          Sourcing for Smarter Hiring
         </h1>
         <p className="text-[var(--text-muted)]">
-          At MployUs, we connect jobseekers with top opportunities and help
-          businesses hire effortlessly.
+          We help you find the right talent, faster. 
+          Through targeted job postings, active search, 
+          and thorough screening, we connect you with 
+          qualified candidates for both full-time and 
+          temporary rolesmatched to your needs, culture, 
+          and timeline.
         </p>
 
         <div className="flex gap-4">
           <button className="button">Request Talents</button>
           <button className="inverseButton">Explore Vacancies</button>
+        </div>
+        <div className="flex flex-row gap-4 text-center text-[var(--text-muted)]">
+          <div><h3>120+</h3> <span>Job post</span></div>
+          <div><h3>64+</h3><span>Available Talents</span></div>
+          <div><h3>50+</h3><span>Successful Placements</span></div>
         </div>
       </div>
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 // src/app/layout.tsx
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Nunito } from 'next/font/google'
 import { AuthProvider } from "./context/authContext";
