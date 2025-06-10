197e4bb64761b6fc1d0a7914e52d596afae9ba95
seyi <seyiduncan40@gmail.com>
2025-06-10 12:55:08 +0100

    hero

--- src/app/components/homepage/hero.tsx
+++ src/app/components/homepage/hero.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Hero() {
   return (
-    <div className="containerDiv flex flex-col lg:flex-row items-center justify-between gap-10">
+    <div className="containerDiv flex flex-col lg:flex-row items-center justify-between gap-20">
       <div className="flex flex-col gap-4 max-w-xl">
         <p className="text-[var(--primary-color)]">Comprehensive Talent Solutions</p>
         <h1>
@@ -31,16 +31,32 @@ export default function Hero() {
       </div>
 
       {/* Fixed height to allow Image fill to work correctly */}
-    <div className="w-full lg:w-1/2">
-      <Image
-        src="/images/teamHeroPic.png"
-        alt="Team Hero Pic"
-        width={630} // or any natural width of your image
-        height={460} // or the natural height, no need to calculate it
-        className="w-full h-auto "
-        priority
-      />
-  </div>
+   <div className="relative w-full p-5 lg:w-1/2">
+  <Image
+    src="/images/backgnd.png"
+    alt="Decor Top Right"
+    width={200}
+    height={200}
+    className="absolute z-0 w-80 h-auto right-0 top-0"
+    priority
+  />
+  <Image
+    src="/images/backgnd.png"
+    alt="Decor Bottom Left"
+    width={200}
+    height={200}
+    className="absolute z-0 w-80 h-auto left-0 bottom-0 transform scale-x-[-1]"
+    priority
+  />
+  <Image
+    src="/images/teamHeroPic2.png"
+    alt="Team Hero Pic"
+    width={630}
+    height={460}
+    className="relative object-contain z-10 left-1/2 transform -translate-x-1/2 mt-2 h-auto"
+    priority
+  />
+</div>
     </div>
   );
 }
